549ea92ec8a9d2f389c29d70b22a9eb433bfc53f
Jake Toan <jaketoan@hotmail.co.uk>
2024-01-07 14:26:02 +0000

    Renames

--- src/player/system/drop_processor.rs
+++ src/player/system/drop_processor.rs
@@ -5,18 +5,23 @@ use bevy::prelude::*;
 pub fn process_drop(
     mut commands: Commands,
     mut event_reader: EventReader<DropEvent>,
-    mut player_query: Query<(&mut CurrentActivity, &Children), With<Player>>,
+    mut player_query: Query<(&mut CurrentActivity, Option<&Children>), With<Player>>,
 ) {
     for event in event_reader.read() {
-        // Getting the player's current activity and entity children
-        let (mut current_activity, children) = player_query.get_mut(event.0).unwrap();
+        // Getting the player's current activity and children (if they exist).
+        let (mut current_activity, children_opt) = player_query.get_mut(event.0).unwrap();
 
         // If the player is already carrying something, pressing this button will make them "drop" it.
         if let Activity::Carrying = current_activity.0 {
-            for child in children.iter() {
-                // Detach the item child from its player parent, but maintain the global transform
-                // of the item.
-                commands.entity(*child).remove_parent_in_place();
+            // This shouldn't ever be `None` whilst `Activity::Carrying` and having the item components
+            // as children are coupled. i.e. you shouldn't be able to have the player state == Carrying
+            // and no children to be on the entity.
+            if let Some(children) = children_opt {
+                for child in children.iter() {
+                    // Detach the item child from its player parent, but maintain the global transform
+                    // of the item.
+                    commands.entity(*child).remove_parent_in_place();
+                }
             }
 
             // Update the player's activity to reflect that they're no longer carrying.
