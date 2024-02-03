5f47e83dbf24971e470cb24af0736e516fdd6de5
Jake Toan <jaketoan@hotmail.co.uk>
2024-02-03 16:40:32 +0000

    Ship it

--- src/network_client/system.rs
+++ src/network_client/system.rs
@@ -4,6 +4,9 @@ use crate::common::EventWrapper;
 use crate::network_client::Client;
 use crate::player::MovementEvent;
 
+// TODO remove this.
+// Once I decouple the "client-side" behaviour from the server-processing, we'll be able to reliably
+// initialise the client and put the event push in the actual system function alongside the event emission.
 #[allow(clippy::type_complexity)]
 pub fn process_move_client(
     mut event_reader: EventReader<MovementEvent>,
